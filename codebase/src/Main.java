ab41316c2bb1d5878e950b59d5f26d22c4938c40
Birk Nergaard <birknergard@gmail.com>
2025-03-29 11:58:56 +0100

    Created scanner in Main to read database file.

--- codebase/src/Main.java
+++ codebase/src/Main.java
@@ -1,6 +1,9 @@
 import DataHandling.City;
 import Sorters.QuickSorter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -9,13 +12,21 @@ public class Main {
     public static void main(String[] args) {
 
         // Handle data import
+        try{
+            LinkedList<City> cities = new LinkedList<>();
+            Scanner reader = new Scanner(new FileInputStream("../worldcities.csv"));
+            System.out.println(reader.next());
 
-        // Parse data into data structure
-        LinkedList<City> cities = new LinkedList<>();
+            // Parse data into data structure
 
-        // Randomize order of list so its unsorted
+            // Randomize order of list so its unsorted
 
-        // Use data structure (LinkedList) in sorting methods
+            // Use data structure (LinkedList) in sorting methods
+
+        } catch (FileNotFoundException e){
+            System.out.println("Could not load file.");
+            return;
+        }
 
         // Bubble sort
 
