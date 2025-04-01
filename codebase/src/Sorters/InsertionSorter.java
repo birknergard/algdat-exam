42485ab0a5558de585614544c2a7d172cd29e2e1
gizuDev <birknergard@gmail.com>
2025-04-01 10:53:48 +0200

    Created testing environment. (main) and boilerplate sort.

--- codebase/src/Sorters/InsertionSorter.java
+++ codebase/src/Sorters/InsertionSorter.java
@@ -1,4 +1,31 @@
 package Sorters;
+import java.util.LinkedList;
+
+/*
+*
+*
+* */
 
 public class InsertionSorter {
+    public boolean sort(){
+
+
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> testList = new LinkedList<>();
+        testList.add(10);
+        testList.add(0);
+        testList.add(20);
+        testList.add(73);
+        testList.add(1);
+        testList.add(50);
+        testList.add(5);
+        testList.add(100);
+        testList.add(4572);
+        testList.add(1);
+        testList.add(2);
+    }
 }
