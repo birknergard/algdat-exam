2a5aae2708b1e92ad48d9a1b3fd219c267a0d83a
gizuDev <birknergard@gmail.com>
2025-04-08 17:32:48 +0200

    Refactored MergeSorter to implement Sorter interface, made methods non-static, created SortData fields for logging.

--- codebase/src/Sorters/MergeSorter.java
+++ codebase/src/Sorters/MergeSorter.java
@@ -3,13 +3,17 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class MergeSorter {
-
+public class MergeSorter implements Sorter{
     //Data fields
+    int partitions;
+    int operations;
 
+    public MergeSorter(){
+       partitions = 0;
+       operations = 0;
+    }
 
-
-    public static void sort(Double[] list) {
+    public void sort(Double[] list, int flag) {
         int listLength = list.length;
         if (listLength < 2) {
             return;
@@ -28,17 +32,15 @@ public class MergeSorter {
         }
 
         //Recursive calls for both halves
-        sort(leftHalf);
-        sort(rightHalf);
+        sort(leftHalf, 0);
+        sort(rightHalf, 0);
 
         //Merging the sorted halves with merge()
         merge(list, leftHalf, rightHalf);
     }
 
-
-
     //Merge function to combine sorted halves
-    private static void merge(Double[] list, Double[] leftHalf, Double[] rightHalf) {
+    private void merge(Double[] list, Double[] leftHalf, Double[] rightHalf) {
         int i = 0, j = 0, k = 0;
         int leftSize = leftHalf.length;
         int rightSize = rightHalf.length;
@@ -61,10 +63,7 @@ public class MergeSorter {
         }
     }
 
-
-
-    public static void main(String[] args) {
-
+    public void internalTest(String[] args) {
         Double[] testList = new Double[10];
         for (int i = 0; i <= 9; i++) {
             testList[i] = (double) (i + 1);
@@ -81,7 +80,7 @@ public class MergeSorter {
         System.out.println("\n");
 
         long startTimeOpt = System.nanoTime();
-        sort(testList);
+        sort(testList, 0);
         long endTimeOpt = System.nanoTime();
         long timeOpt = endTimeOpt - startTimeOpt;
 
